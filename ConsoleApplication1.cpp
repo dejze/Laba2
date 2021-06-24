35ee63e7d6e9a4392ff2f2c17ac0e7201eb3cf1d
dejze <76784874+dejze@users.noreply.github.com>
2021-06-24 18:43:13 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <time.h>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    srand(time(NULL));
+    int length;
+    cout << "Введите длину пароля: ";
+    cin >> length;
+    {
+        for (int j = 0; j < length; j++) {
+            cout << static_cast<char>(rand() % 48 + 48) << "";
+        }
+        cout << endl;
+    }
+    system("PAUSE");
+    return 0;
+}
